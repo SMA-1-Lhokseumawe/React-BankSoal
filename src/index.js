8c9184223198c8e343326ee1471a7d9a52b4b960
IrfanNasyakban <irvan.26102001@gmail.com>
2024-11-11 16:26:05 +0700

    add loginpage with api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 
 import './index.css';
 import App from './App';
+import axios from 'axios';
 import { ContextProvider } from './contexts/ContextProvider';
 
+axios.defaults.withCredentials = true;
+
 ReactDOM.render(
-    <ContextProvider>
-        <App />
-    </ContextProvider>,
+        <Provider store={store}>
+            <ContextProvider>
+                <App />
+            </ContextProvider>
+        </Provider>,
     document.getElementById('root')
 );
